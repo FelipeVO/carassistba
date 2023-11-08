fcf0b6a1ccc001b4a259764f1f8015ee47e9b3ac
felipevo <varelaolidfelipe@gmail.com>
2023-11-08 19:59:59 -0300

    miercoles 8 2

--- js/funciones.js
+++ js/funciones.js
@@ -16,3 +16,25 @@ const tl = gsap.timeline({
 tl.to('.nav-bg', {
     opacity: 1
 })
+
+// Esconder Hash
+
+function myFunction () {
+    // get the anchor link buttons
+    const menuBtn = $('.scroll-btn');
+    // when each button is clicked
+    menuBtn.click(()=>{	
+      // set a short timeout before taking action
+      // so as to allow hash to be set
+      setTimeout(()=>{
+        // call removeHash function after set timeout
+        removeHash();
+      }, 5); // 5 millisecond timeout in this case
+    });
+  
+    // removeHash function
+    // uses HTML5 history API to manipulate the location bar
+    function removeHash(){
+      history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
+    }
+  };
